3cbae56a24e8dad260281f14ee3ae4e4485ac4ec
Jose Villaverde <jlvbalsa@gmail.com>
2022-09-11 08:51:02 +0200

    handking validation errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
+/* ticketing.dev/api/users/signup */
 router.post(
   '/api/users/signup',
   [
@@ -13,7 +14,18 @@ router.post(
       .withMessage('Password must be between 4 and 20 characters'),
   ],
   (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
     const { email, password } = req.body;
+
+    console.log('Creating a user...');
+
+    res.send({});
+
+    // new User({ email, password })
   }
 );
 
