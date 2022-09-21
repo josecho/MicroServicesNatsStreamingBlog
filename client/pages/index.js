e505431b85d48a57d19cc5e09dbe5cf62916fb9c
Jose Villaverde <jlvbalsa@gmail.com>
2022-09-21 09:18:12 +0200

    Specifying the Host

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,23 +3,29 @@ import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
-  /*  axios.get('/api/users/currentuser').catch((err) => {
-     console.log(err.message);
-   }); */
-
   return <h1>Landing Page</h1>;
 };
 
 LandingPage.getInitialProps = async () => {
-  /* const response = await axios.get('/api/users/currentuser');
-  return response.data;
- */
   if (typeof window === 'undefined') {
     // we are on the server!
     // request sho/ingress-nginx-controller.ingress-nginx.....
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    );
+    return data;
   } else {
     // we are on the browser!
     // request can be made  with a base url of ''
+    const response = await axios.get('/api/users/currentuser').catch((err) => {
+      console.log(err.message);
+    });
+    return response.data;
 
   }
   return {};
