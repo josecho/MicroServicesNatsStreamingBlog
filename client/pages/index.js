a00c6195499f8b85b6f898edcc368dcc907bdac0
Jose Villaverde <jlvbalsa@gmail.com>
2022-09-21 08:56:03 +0200

    On the Server or the Browser

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,28 @@
-const Index = () => {
-  return <h1>Landing page</h1>
-}
+import axios from 'axios';
 
-export default Index;
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  /*  axios.get('/api/users/currentuser').catch((err) => {
+     console.log(err.message);
+   }); */
+
+  return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async () => {
+  /* const response = await axios.get('/api/users/currentuser');
+  return response.data;
+ */
+  if (typeof window === 'undefined') {
+    // we are on the server!
+    // request sho/ingress-nginx-controller.ingress-nginx.....
+  } else {
+    // we are on the browser!
+    // request can be made  with a base url of ''
+
+  }
+  return {};
+};
+
+export default LandingPage;
