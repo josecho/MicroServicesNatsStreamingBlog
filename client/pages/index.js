681aec76a163d50db418e3310dde9c7853c4b8e3
Jose Villaverde <jlvbalsa@gmail.com>
2022-09-21 09:29:21 +0200

    Passing Through the Cookies

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,16 +6,14 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
   if (typeof window === 'undefined') {
     // we are on the server!
     // request sho/ingress-nginx-controller.ingress-nginx.....
     const { data } = await axios.get(
       'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
       {
-        headers: {
-          Host: 'ticketing.dev',
-        },
+        headers: req.headers
       }
     );
     return data;
