a5bfa3628865100f1b1fb48bacc8ae6f236b725c
Jose Villaverde <jlvbalsa@gmail.com>
2022-09-22 11:09:27 +0200

    Building the Header

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,13 +4,15 @@ const LandingPage = ({ currentUser }) => {
   return currentUser ? (
     <h1>You are signed in</h1>
   ) : (
-    <h1>You are not signed in</h1>);
+    <h1>You are NOT signed in</h1>
+  );
 };
 
-LandingPage.getInitialProps = async (context) => {
-  console.log('LANDING PAGE')
+LandingPage.getInitialProps = async context => {
+  console.log('LANDING PAGE!');
   const client = buildClient(context);
   const { data } = await client.get('/api/users/currentuser');
+
   return data;
 };
 
