8fbdb1211e3a354c2be54cb5cb83cc4de5b170a8
Jose Villaverde <jlvbalsa@gmail.com>
2022-09-28 11:37:46 +0200

    Validating Title and Price

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,4 +1,3 @@
-import { requireAuth } from '@jlvbcooptickets/common';
 import request from 'supertest';
 import { app } from '../../app';
 
@@ -9,9 +8,7 @@ it('has a route handler listening to /api/tickets for post requests', async () =
 });
 
 it('can only be accessed if the user is signed in', async () => {
-  const response = await request(app).post('/api/tickets').send({});
-
-  expect(response.status).toEqual(401);
+  await request(app).post('/api/tickets').send({}).expect(401);
 });
 
 it('returns a status other than 401 if the user is signed in', async () => {
@@ -23,8 +20,42 @@ it('returns a status other than 401 if the user is signed in', async () => {
   expect(response.status).not.toEqual(401);
 });
 
-it('returns an error if an invalid title is provided', async () => {});
+it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10,
+    })
+    .expect(400);
 
-it('returns an error if an invalid price is provided', async () => {});
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10,
+    })
+    .expect(400);
+});
+
+it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'asldkjf',
+      price: -10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'laskdfj',
+    })
+    .expect(400);
+});
 
 it('creates a ticket with valid inputs', async () => {});
