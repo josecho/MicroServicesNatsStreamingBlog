7c8172a01f4404b8103a0997e8c18292647942d9
Jose Villaverde <jlvbalsa@gmail.com>
2022-09-28 14:18:14 +0200

    Complete Index Route Implementation

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -9,6 +9,7 @@ import {
 } from '@jlvbcooptickets/common';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
 
 const app = express();
 app.set('trust proxy', true);
@@ -24,6 +25,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
