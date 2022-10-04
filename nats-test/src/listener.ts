a33a5fb213732bf7ac6147df380944afd11d353f
josecho <jlvbalsa@gmail.com>
2022-10-04 12:54:38 +0200

    Client ID Generation

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,17 +1,23 @@
-import nats from 'node-nats-streaming';
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
 
 console.clear();
 
-const stan = nats.connect('ticketing', '123', {
-  url: 'http://localhost:4222',
-});
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+    url: 'http://localhost:4222',
+  });
+  
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
   const subscription = stan.subscribe('ticket:created');
 
-  subscription.on('message', (msg) => {
-    console.log('Message recieved');
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
   });
 });
