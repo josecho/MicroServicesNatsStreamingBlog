4e0140e3b0320299c0c961b24862b4145d2a8dc5
josecho <jlvbalsa@gmail.com>
2022-10-04 13:17:29 +0200

    Queue Groups

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -5,19 +5,22 @@ console.clear();
 
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
     url: 'http://localhost:4222',
-  });
-  
+});
+
 
 stan.on('connect', () => {
-  console.log('Listener connected to NATS');
+    console.log('Listener connected to NATS');
 
-  const subscription = stan.subscribe('ticket:created');
+    const subscription = stan.subscribe(
+        'ticket:created',
+        'orders-service-queue-group'
+    );
 
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
+    subscription.on('message', (msg: Message) => {
+        const data = msg.getData();
 
-    if (typeof data === 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-  });
+        if (typeof data === 'string') {
+            console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+        }
+    });
 });
