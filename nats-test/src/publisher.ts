5c92872d4b0120ad730ace4b1c326d22ee168702
josecho <jlvbalsa@gmail.com>
2022-10-04 10:21:26 +0200

    Building a NATS Test Project

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,12 @@
+import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
+
+console.clear();
+
+const stan = nats.connect('ticketing', 'abc', {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', async () => {
+  console.log('Publisher connected to NATS');
+});
