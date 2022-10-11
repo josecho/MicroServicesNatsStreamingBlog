3c63b5dffbbc7ad0263c5298bb731e60a507e6ce
josecho <jlvbalsa@gmail.com>
2022-10-11 08:17:34 +0200

    Enforcing data types

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -8,7 +8,7 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
     readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
     queueGroupName = 'payment-service';
 
-    onMessage(data: any, msg: Message): void {
+    onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
         console.log('Event data!', data);
 
         msg.ack();
