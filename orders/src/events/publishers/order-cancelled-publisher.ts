b093eaabe1bba3e3f41aebae9a23dcb8c8ff53e8
josecho <jlvbalsa@gmail.com>
2022-10-18 09:14:10 +0200

    Publishing the Order Creation

--- orders/src/events/publishers/order-cancelled-publisher.ts
+++ orders/src/events/publishers/order-cancelled-publisher.ts
@@ -2,4 +2,4 @@ import {Publisher, OrderCancelledEvent , Subjects} from '@jlvbcooptickets/common
 
 export class OrderCancelledPublisher extends Publisher<OrderCancelledEvent> {
     subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
-}
+  }
--- orders/src/events/publishers/order-created-publisher.ts
+++ orders/src/events/publishers/order-created-publisher.ts
@@ -1,5 +1,5 @@
-import { Subjects, Publisher, OrderCancelledEvent } from '@jlvbcooptickets/common';
+import { Publisher, OrderCreatedEvent, Subjects } from '@jlvbcooptickets/common';
 
-export class OrderCancelledPublisher extends Publisher<OrderCancelledEvent> {
-  subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+export class OrderCreatedPublisher extends Publisher<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
 }
