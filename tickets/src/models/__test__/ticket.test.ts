50d3a2ea2cdd04e4e867d0ef8d51ebe24db9cb45
josecho <jlvbalsa@gmail.com>
2022-10-19 09:08:49 +0200

    Testing OCC

---
+++ tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,38 @@
+import { Ticket } from '../ticket';
+
+it('implements optimistic concurrency control', async () => {
+    // Create an instance of a ticket
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 5,
+        userId: '123',
+    });
+
+    // Save the ticket to the database
+    await ticket.save();
+
+    // fetch the ticket twice
+    const firstInstance = await Ticket.findById(ticket.id);
+    const secondInstance = await Ticket.findById(ticket.id);
+
+    // make two separate changes to the tickets we fetched
+    firstInstance!.set({ price: 10 });
+    secondInstance!.set({ price: 15 });
+
+    // save the first fetched ticket
+    await firstInstance!.save();
+
+    // I found that there are some interoperation issues between Jest and TypeScript.
+    /*  expect(async () =>{
+       await secondInstance?.save();
+     }).toThrow(); */
+
+    // save the second fetched ticket and expect an error
+    try {
+        await secondInstance!.save();
+    } catch (err) {
+        return;
+    }
+
+    throw new Error('Should not reach this point');
+});
