d5914d02f09386e5b872c7c9aee0dca1102cbdb8
josecho <jlvbalsa@gmail.com>
2022-10-31 10:56:42 +0100

    Creating a Charge with Stripe

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -8,6 +8,7 @@ import {
   NotFoundError,
   OrderStatus
 } from '@jlvbcooptickets/common';
+import { stripe } from '../stripe';
 import { Order } from '../models/order';
 
 const router = express.Router();
@@ -32,6 +33,12 @@ router.post(
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError('Cannot pay for an cancelled order');
     }
+
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token,
+    });
 
     res.send({ success: true });
   }
