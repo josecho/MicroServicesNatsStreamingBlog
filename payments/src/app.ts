7a632fb9c488f427fbd6984f97ec22942b465d9c
josecho <jlvbalsa@gmail.com>
2022-10-31 09:18:46 +0100

    Implementing the Create Charge Handler

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -7,6 +7,8 @@ import {
   NotFoundError,
   currentUser,
 } from '@jlvbcooptickets/common';
+import { createChargeRouter } from './routes/new';
+
 
 const app = express();
 app.set('trust proxy', true);
@@ -19,6 +21,8 @@ app.use(
 );
 
 app.use(currentUser);
+
+app.use(createChargeRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
