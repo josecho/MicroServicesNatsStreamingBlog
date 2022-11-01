a9fdecdf6dee774dac5634542922c035e27c3529
josecho <jlvbalsa@gmail.com>
2022-11-01 08:08:01 +0100

    Mocked Stripe Client

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,7 @@ import request from 'supertest';
 import { OrderStatus } from '@jlvbcooptickets/common';
 import { app } from '../../app';
 import { Order } from '../../models/order';
+import {stripe} from '../../stripe';
 
 jest.mock('../../stripe');
 
@@ -75,5 +76,12 @@ it('returns a 400 when purchasing a cancelled order', async () => {
       .send({
         token: 'tok_visa',
         orderId: order.id,
-      });
+      })
+      .expect(201);
+
+    const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+    expect(chargeOptions.source).toEqual('tok_visa');
+    expect(chargeOptions.amount).toEqual(20 * 100);
+    expect(chargeOptions.currency).toEqual('usd');
+
   });
